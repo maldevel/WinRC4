8d6dafa25cc88524974207a18a07f1f1c6cd44ce
maldevel <maldevel@mail.com>
2016-04-06 09:34:22 +0300

    base64
    base64

---
+++ WinRC4/main.c
@@ -0,0 +1,60 @@
+/*
+WinRC4 - RC4 Encryption with CryptoAPI and C
+Copyright (C) 2016  @maldevel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Windows.h>
+#include <stdio.h>
+#include "common.h"
+#include "base64.h"
+
+int main(int argc, char **argv)
+{
+	char *encoded = 0;
+	unsigned long encodedLen = 0, decodedLen = 0;
+	char *decoded = 0;
+	WCHAR *encodedW = 0;
+
+	if (argc != 2)
+	{
+		printf("usage: WinRC4.exe <string to encrypt>\n");
+		return EXIT_FAILURE;
+	}
+
+	if (Base64EncodeA(&encoded, &encodedLen, argv[1], strlen(argv[1])))
+	{
+		printf("Base64 encoded: %s\n", encoded);
+		if (Base64DecodeA(&decoded, &decodedLen, encoded, encodedLen))
+		{
+			printf("Base64 decoded: %s\n", decoded);
+			SAFE_FREE(encoded);
+			SAFE_FREE(decoded);
+		}
+	}
+
+	if (Base64EncodeW(&encodedW, &encodedLen, "tester", 6))
+	{
+		wprintf(L"Base64 encoded: %s\n", encodedW);
+		if (Base64DecodeW(&decoded, &decodedLen, encodedW, encodedLen))
+		{
+			printf("Base64 decoded: %s\n", decoded);
+			SAFE_FREE(encodedW);
+			SAFE_FREE(decoded);
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ WinRC4/rc4.c
@@ -0,0 +1,49 @@
+/*
+WinRC4 - RC4 Encryption with CryptoAPI and C
+Copyright (C) 2016  @maldevel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Windows.h>
+#include <Wincrypt.h>
+#define MY_ENCODING_TYPE  (PKCS_7_ASN_ENCODING | X509_ASN_ENCODING)
+#include "rc4.h"
+
+bool CryptoInit(HCRYPTPROV *hCryptProv)
+{
+	if (!CryptAcquireContextW(hCryptProv, NULL, NULL, PROV_RSA_FULL, 0))
+	{
+		if (GetLastError() == NTE_BAD_KEYSET)
+		{
+			if (!CryptAcquireContextW(hCryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_NEWKEYSET))
+			{
+				printf("Error: %d\n", GetLastError());
+				return false;
+			}
+		}
+		else
+		{
+			printf("Error: %d\n", GetLastError());
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void CryptoUninit(HCRYPTPROV hCryptProv)
+{
+	if (hCryptProv)	if (!CryptReleaseContext(hCryptProv, 0)) printf("Error: %d\n", GetLastError());
+}
